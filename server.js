b0eab03478701f273365fecc8d065a0468828cbb
Hamza Halilovic <hamza@Hamzas-MacBook-Pro.local>
2020-04-16 15:38:49 +0200

    user requrest and body

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 //connect database
 connectDB();
 
+//init middleware = allows to get the data in req.body
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API Running"));
 
 //routes define
