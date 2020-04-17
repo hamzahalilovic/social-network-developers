870711c67fb6ee290e4aaec3134cb679cf11abd5
Hamza Halilovic <hamza@Hamzas-MacBook-Pro.local>
2020-04-17 22:28:52 +0200

    profile model and current profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator/check");
 
-// @route GET api/auth
-// @desc  test route
-// @access public
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
-router.get("/", (req, res) => res.send("profile route"));
+// @route GET api/profile/me
+// @desc  get current user's profile
+// @access private
+
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
+
+    //check to see if there is no profile
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ msg: "there is no profile for this user " });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server errro");
+  }
+});
+
+// @route POST api/profile
+// @desc  create or update user profile
+// @access private
 
 module.exports = router;
