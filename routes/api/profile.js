b114ac6cb119f998f163d7cba650f61649a9cec0
Hamza Halilovic <hamza@Hamzas-MacBook-Pro.local>
2020-04-20 23:03:52 +0200

    delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -161,4 +161,23 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+// @route DELETE api/profile
+// @desc  delete profile, user and posts
+// @access private
+
+router.delete("/", auth, async (req, res) => {
+  try {
+    //remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    // remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: "User is deleted" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
+  }
+});
+
 module.exports = router;
